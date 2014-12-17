7c02153ca864c613971f8ca076a55db77a00c8a1
EddM <eddm@me.com>
2014-12-17 21:42:24 +0000

    log that shit

--- proxy.rb
+++ proxy.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 require "json"
 
+configure do
+  # logging is enabled by default in classic style applications,
+  # so `enable :logging` is not needed
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
 post "/builds" do
   payload_data = JSON.parse(params[:payload])
 end
